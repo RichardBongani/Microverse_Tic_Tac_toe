180a549bbc64092eb64e9a8fbf54558c9ffeafe6
Richard <phalarichard1@gmail.com>
2020-02-03 21:47:10 +0200

    Fixing and adding TODO's suggested by TSE's

--- bin/main.rb
+++ bin/main.rb
@@ -7,21 +7,19 @@ puts "Player one enter your name: "
 player_one = gets.chomp
 puts "Player two enter your name: "
 player_two = gets.chomp
-# TODO assign players their respective symbols
-puts "Player one plays X and player two plays O"
-# TODO create the board
-board = [0,1,2,3,4,5,6,7,8]
-puts " #{board[0..2]},\n #{board[3..5]},\n #{board[6..9]}"
-# TODO Tell which player turn it is and ask player to choose position
-puts "Player one, choose position between 1-9: "
-position_p1 = gets.chomp
-puts "Player two, choose position between 1-9: "
-position_p2 = gets.chomp
-# TODO check if it is a valid position
-# if position is valid, number = player_symbol. Else check if board has empty positions.
-# TODO if board has empty positions ask player to choose a valid position
-# TODO check if it is a winning move or a draw.
-# Else if we still having empty positions
-# Ask new position to the player. If we don't check if it's a draw
-# TODO if pass check repeat loop starting from show board with the new positions
-# TODO if didn't pass the check show message: "You win" or "You lose"
+
+#Repetative loop:
+game  = true
+while game
+  #TODO create a class for the board and add a method defition
+  board = [0,1,2,3,4,5,6,7,8]
+  puts " #{board[0..2]},\n #{board[3..5]},\n #{board[6..8]}"
+  #TODO creating a method for player position
+  puts "Player one, choose position between 0-8: "
+  position_p1 = gets.chomp
+  puts "Player two, choose position between 0-8: "
+  position_p2 = gets.chomp
+  puts "Player one plays X and player two plays O"
+  #TODO check if players are entering valid positions
+  #TODO create one method to check if player wins or it is a draw
+end
