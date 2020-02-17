d5023429eb243d23689f16496171ce35086ba0ed
eduardo <eduardoreisalvarenga@gmail.com>
2020-02-17 17:08:25 -0300

    Adding changes to main. While loop

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require_relative '../lib/board.rb'
 
-def board
+def board(board_index = [0,1,2,3,4,5,6,7,8])
   puts "#{@board_index[0]}    |#{@board_index[1]}   |#{@board_index[2]}      "
   puts "-----------------"
   puts "#{@board_index[3]}    |#{@board_index[4]}   |#{@board_index[5]}      "
@@ -11,23 +11,40 @@ end
 
 def players
   puts "Welcome to Tic Tac Toe"
+  puts "Player one start playing. Player one use X and player two use O"
+  player_info = []
   2.times do |i|
-    print "Player #{i+1} name:"
+    print "Player #{i+1} name: "
     player_name = gets.chomp
+    player_info << player_name
+    player_info << "X" if i == 0
+    player_info << "O" if i == 1
   end
+  return player_info
 end
 
-
-
-
+# TODO send information to board
+# Receive and display new board
 def game_state
+  board
+  players_info = players
   game = Board.new
   game_on = true
   while game_on
-    print "Player one, pick position: "
-    position = gets.chomp.to_i
-    print "Player two, pick position: "
-    position_two = gets.chomp.to_i
-    
+    2.times do |i|
+      if i == 0
+        puts "#{players_info[0]}'s turn"
+        puts "Pick a position: "
+        position = gets.chomp.to_i
+        symbol = players_info[1]
+      else
+        puts "#{players_info[2]}'s turn"
+        puts "Pick a position: "
+        position = gets.chomp.to_i
+        symbol = players_info[3]
+      end
+      array = check(position, symbol)
+    end
+    board(array)
   end
 end
