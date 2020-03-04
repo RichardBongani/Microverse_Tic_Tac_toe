831ab1d6291d22da6c94229e658034ed5ddb9680
Richard <phalarichard1@gmail.com>
2020-03-04 21:52:42 +0200

    running test for tic_tac_toe_spec.rb method

---
+++ spec/tic_tac_toe_spec.rb
@@ -0,0 +1,15 @@
+# require "rspec/autorun"
+require_relative "../lib/tic_tac_toe.rb"
+
+describe TicTacToe do
+  let(:tictac_test){TicTacToe.new}
+  it "returns a boolean(true/false) after checking if position is valid" do 
+    expect(tictac_test.valid_position?([0..8],0)).to be(true).or be(false)
+    expect(tictac_test.valid_position?([0..8],1)).to be(true).or be(false)
+  end
+
+  it "checks for winning moves" do
+    expect(tictac_test.all_equal?(["X","X","X"])).to be(true)
+    expect(tictac_test.all_equal?(["O","X","X"])).to be(false)
+  end
+end
