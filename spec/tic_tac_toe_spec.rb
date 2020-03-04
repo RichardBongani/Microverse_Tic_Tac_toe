bd154010c0588370402206dbbd46717c2c7cc770
eduardo <eduardoreisalvarenga@gmail.com>
2020-03-04 18:58:42 -0300

    Rspec testcase:board

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,8 +1,10 @@
+require_relative "../bin/main.rb"
 require_relative "../lib/tic_tac_toe.rb"
+require_relative "../lib/player.rb"
 
 describe TicTacToe do
   let(:tictac_test){TicTacToe.new}
-  it "returns a boolean(true/false) after checking if position is valid" do 
+  it "returns a boolean(true/false) after checking if position is valid" do
     expect(tictac_test.valid_position?([0..8],0)).to be(true).or be(false)
     expect(tictac_test.valid_position?([0..8],1)).to be(true).or be(false)
   end
@@ -10,5 +12,17 @@ describe TicTacToe do
   it "checks for winning moves" do
     expect(tictac_test.all_equal?(["X","X","X"])).to be(true)
     expect(tictac_test.all_equal?(["O","X","X"])).to be(false)
+  end
+
+  it "pass values to all_equal method" do
+    expect(tictac_test.winning_move?(%w[X X X O X O X O X])).to eq("Win")
+    expect(tictac_test.winning_move?(%w[X O O O X X X X O])).to eq("Draw")
+  end
+end
+
+describe Main do
+  start_playing = Main.new
+  it "checks if the board is displaying" do
+    expect(start_playing.board_display())
   end
 end
